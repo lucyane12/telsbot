538caa6022275f836966971d77ad9fe0dc703ef7
lucyane12 <129069987+lucyane12@users.noreply.github.com>
2023-07-30 19:44:40 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const telegraf_1 = require("telegraf");
+const bot = new telegraf_1.Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => {
+    ctx.reply('Hello ' + ctx.from.first_name + '!');
+});
+bot.help((ctx) => {
+    ctx.reply('Send /start to receive a greeting');
+    ctx.reply('Send /keyboard to receive a message with a keyboard');
+    ctx.reply('Send /quit to stop the bot');
+});
+bot.command('quit', (ctx) => {
+    // Explicit usage
+    ctx.telegram.leaveChat(ctx.message.chat.id);
+    // Context shortcut
+    ctx.leaveChat();
+});
+bot.command('keyboard', (ctx) => {
+    ctx.reply('Keyboard', Markup.inlineKeyboard([
+        Markup.button.callback('First option', 'first'),
+        Markup.button.callback('Second option', 'second'),
+    ]));
+});
+bot.on('text', (ctx) => {
+    ctx.reply('You choose the ' +
+        (ctx.message.text === 'first' ? 'First' : 'Second') +
+        ' Option!');
+});
+bot.launch();
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
+//# sourceMappingURL=app.js.map
